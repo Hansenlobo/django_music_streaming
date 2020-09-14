99c68c644204f93b49b9aafbdff52cb0238ba33b
Hansenlobo <Hansenlobo07@gmail.com>
2020-09-14 19:19:06 +0530

    authentication added

---
+++ authentication/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns=[   
+    # authentication
+    path('register/',views.register, name="register"),
+    path('login/',views.loginP,name="login"),
+    path('logout/', views.logoutPage, name="logout"),
+
+    # password reset
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name="authentication/password_reset.html"),name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="authentication/password_reset_sent.html"),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="authentication/password_reset_form.html"),name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="authentication/password_reset_done.html"), name="password_reset_complete"),
+]
